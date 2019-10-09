977021188c5fac5a21b715b7940ddb2f38fb5aae
mekingme <mekingme@gmail.com>
2019-10-09 20:55:32 +0900

    [HOME] add base image

---
+++ lib/Pages/Mainpage.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class Mainpage extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+/*    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
+
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Color(0xFFFA7A86) : Color(0xFF3F3D62);
+    Color textColor = data['isDaytime'] ? Colors.black : Colors.black;
+*/
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.yellow),
+        primarySwatch: Colors.purple,
+    ),
+      home: MyHomePage(),
+          );
+      }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Card view'),
+      ),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          return Container(
+            height: 130,
+            child: Card(
+//                color: Colors.blue,
+              elevation: 10,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                      onTap: () {
+
+                      },
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            image: DecorationImage(
+                                image: AssetImage('images/simple_background.jpeg'),
+                                fit: BoxFit.cover),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 7.0, color: Colors.black)
+                            ]),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      return showDialog<void>(
+                        context: context,
+                          barrierDismissible: false,
+                          builder: (BuildContext conext) {
+                          return AlertDialog(
+                            title: Text('Not in stock'),
+                            content:
+                                const Text('This item is no longer available'),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text('Ok'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(30.0),
+                        child: Chip(
+                          label: Text('@anonymous'),
+                          shadowColor: Colors.blue,
+                          backgroundColor: Colors.green,
+                          elevation: 10,
+                          autofocus: true,
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
